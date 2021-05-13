b00d86d4998c90ac9045bec909635b407055bf4f
Sergej Serdjukow <62400556+serdjukow@users.noreply.github.com>
2021-05-13 17:24:47 +0200

    U-2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, {useEffect, useState} from 'react'
 import PostsList from './Posts/PostsList'
 
 function App() {
-
     const [posts, setPosts] = useState([])
-
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
@@ -19,11 +17,35 @@ function App() {
             })
     }, [])
 
+    const increment = (id) => {
+        const arr = [...posts]
+        const idx = arr.findIndex(p => p.id === id)
+        if (idx !== -1) {
+            const postItem = {...arr[idx]}
+            postItem.countUp++
+            arr[idx] = postItem
+        }
+        setPosts(arr)
+    }
+    const decrement = (id) => {
+        const arr = [...posts]
+        const idx = arr.findIndex(p => p.id === id)
+        if (idx !== -1) {
+            const postItem = {...arr[idx]}
+            postItem.countDown--
+            arr[idx] = postItem
+        }
+        setPosts(arr)
+    }
 
     return (
         <div className='wrapper'>
             <h1>Posts</h1>
-            <PostsList posts={posts}/>
+            <PostsList
+                posts={posts}
+                increment={increment}
+                decrement={decrement}
+            />
         </div>
     )
 }
