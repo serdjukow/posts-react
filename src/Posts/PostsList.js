98d268c0278b2fd488b13c0fea0f689c20adba18
Sergej Serdjukow <62400556+serdjukow@users.noreply.github.com>
2021-05-13 17:25:27 +0200

    U-2

--- src/Posts/PostsList.js
+++ src/Posts/PostsList.js
@@ -2,11 +2,17 @@ import React from "react";
 import './posts.scss'
 import PostItem from './PostItem'
 
-export default function PostsList(props) {
-    console.log(props.posts[0])
+export default ({posts, increment, decrement}) => {
     return (
         <div className='posts-list'>
-            {props.posts.map(post => <PostItem post={post} key={post.id}/> )}
+            {posts.map(post => (
+                <PostItem
+                    post={post}
+                    key={post.id}
+                    increment={increment}
+                    decrement={decrement}
+                />
+            ))}
         </div>
     )
 }
