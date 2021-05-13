6a02020c639d81d1c1fdaa04ef56d2db90b97a47
Sergej Serdjukow <62400556+serdjukow@users.noreply.github.com>
2021-05-13 17:26:08 +0200

    Update PostItem.js

--- src/Posts/PostItem.js
+++ src/Posts/PostItem.js
@@ -1,42 +1,36 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {imgUp, imgDown, heart, heartBroken} from './PostIcons'
 
-export default function PostItem({post}) {
-    const [countUp, setCountUp] = useState(0)
-    const [countDown, setCountDown] = useState(0)
-
-    const increment = () => setCountUp(countUp + 1)
-    const decrement = () => setCountDown(countDown - 1)
-    const heartSum = (sum) => (sum >= 0) ? heart : heartBroken
-
+export default ({post, increment, decrement}) => {
     return <article className='post-item'>
         <h2>{post.title}</h2>
         <div className='post-content'>{post.body}</div>
+
         <div className='post-counter'>
             <div className='btn-container'>
                 <button className='btn-up'
-                    onClick={() => increment(post.id)}>
+                        onClick={() => increment(post.id)}>
                     {imgUp}
                 </button>
                 <span className='currentUp'>
-                    {countUp}
+                    {post.countUp}
                 </span>
             </div>
             <div className='like-sum'>
                 <span className='like-sum-icon'>
-                    {heartSum(countUp + countDown)}
+                    {(post.countUp + post.countDown) >= 0 ? heart : heartBroken}
                 </span>
                 <span className='like-sum'>
-                    {countUp + countDown}
+                    {post.countUp + post.countDown}
                 </span>
             </div>
             <div className='btn-container'>
                 <button className='btn-down'
-                    onClick={() => decrement(post.id)}>
+                        onClick={() => decrement(post.id)}>
                     {imgDown}
                 </button>
                 <span className='currentDown'>
-                    {countDown}
+                    {post.countDown}
                 </span>
             </div>
         </div>
