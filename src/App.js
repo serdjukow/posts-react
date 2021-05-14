24d5e3ed1dc23e11c90e6a6a021948876f7a16ea
Sergej Serdjukow <62400556+serdjukow@users.noreply.github.com>
2021-05-14 13:35:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,59 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, createContext} from 'react'
 import PostsList from './Posts/PostsList'
+import './Posts/posts.scss'
+
+export const PostsContext = createContext(undefined, undefined)
 
 function App() {
     const [posts, setPosts] = useState([])
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-            .then(postList => {
-                setPosts(postList.map(post => {
-                    return {
-                        ...post,
-                        countUp: 0,
-                        countDown: 0
-                    }
-                }))
-            })
+    useEffect(async () => {
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/posts/?_limit=15`
+        )
+        const postList = await response.json()
+        setPosts(postList.map(post => {
+            return {
+                ...post,
+                countUp: null,
+                countDown: null
+            }
+        }))
     }, [])
 
     const increment = (id) => {
-        const arr = [...posts]
-        const idx = arr.findIndex(p => p.id === id)
-        if (idx !== -1) {
-            const postItem = {...arr[idx]}
-            postItem.countUp++
-            arr[idx] = postItem
-        }
-        setPosts(arr)
+        counter(id, true)
     }
     const decrement = (id) => {
+        counter(id, false)
+    }
+    const counter = (id, isLike) => {
         const arr = [...posts]
         const idx = arr.findIndex(p => p.id === id)
         if (idx !== -1) {
             const postItem = {...arr[idx]}
-            postItem.countDown--
+            if (isLike) {
+                postItem.countUp++
+            } else if (!isLike) {
+                postItem.countUp--
+            }
             arr[idx] = postItem
         }
         setPosts(arr)
     }
 
-    return (
-        <div className='wrapper'>
-            <h1>Posts</h1>
+    return <div className='wrapper'>
+        <h1>Posts</h1>
+        <PostsContext.Provider value={
+            {
+                increment,
+                decrement
+            }
+        }>
             <PostsList
                 posts={posts}
-                increment={increment}
-                decrement={decrement}
             />
-        </div>
-    )
+        </PostsContext.Provider>
+    </div>
 }
 
 export default App
